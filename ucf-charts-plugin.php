66a8761f81078d1184e096c1940598a8dfb76e37
Jim Barnes <jimbarnesdeveloper@gmail.com>
2017-10-04 11:31:42 -0400

    Initial commit

---
+++ ucf-charts-plugin.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: UCF Charts Plugin
+Version: 0.0.1
+Author: UCF Web Communications
+Description: Provides shortcode for creating chart.js charts.
+Plugin URL: https://github.com/UCF/UCF-Charts-Plugin/
+Tags: chart.js,shortcodes
+*/
+if ( ! defined( WP_INC ) ) {
+    die;
+}
+
+
+define( 'UCF_CHARTS__PLUGIN_FILE', __FILE__ );
+define( 'UCF_CHARTS__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
---
+++ uninstall.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Handles the uninstallation of the plugin
+ */
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    die;
+}
